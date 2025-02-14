58dbbd4267ac622d487340f1109d4be0363cd8b0
f-trycua <f@trycua.com>
2025-02-14 15:31:58 +0100

    Update to v0.1.9

--- Formula/lume.rb
+++ Formula/lume.rb
@@ -1,8 +1,8 @@
 class Lume < Formula
   desc "CLI and local API server to run macOS and Linux VMs on Apple Silicon"
   homepage "https://github.com/trycua"
-  url "https://github.com/trycua/lume/releases/download/v0.1.8/lume.tar.gz"
-  sha256 "5ba8506ae9553936d00c127a8e3a6eb26e8606ea9bd2c05baba55290125372b2"
+  url "https://github.com/trycua/lume/releases/download/v0.1.9/lume.tar.gz"
+  sha256 "824eee499d7101bfe6327dd4534fc43189807f1b2d8edee96824400c35aab192"
   license "MIT"
 
   depends_on :macos
