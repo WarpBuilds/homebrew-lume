436bb377aba896f05c76f43b2477d9f988ee6470
PrashantRaj18198 <prashant@argonaut.dev>
2025-04-15 09:41:49 +0530

    change sha for fix

--- Formula/lume.rb
+++ Formula/lume.rb
@@ -2,7 +2,7 @@ class Lume < Formula
   desc "CLI and local API server to run macOS and Linux VMs on Apple Silicon"
   homepage "https://github.com/trycua"
   url "https://github.com/trycua/lume/releases/download/lume-v0.1.14/lume.tar.gz"
-  sha256 "95891f3373323a37aa76be66a2e01e13189c2c76d11debb9492c091e0de4951f"
+  sha256 "9a488a0f8bdbb10d21cb1c73af4bb7220853860c9231f82128193c8701b99dfe"
   license "MIT"
 
   depends_on :macos
