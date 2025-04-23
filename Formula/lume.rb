d70e2dff419c4fcbc4ffa205f7a465b7749d040a
Prashant Raj <prashant@argonaut.dev>
2025-04-23 12:02:23 +0530

    Update lume.rb

--- Formula/lume.rb
+++ Formula/lume.rb
@@ -2,7 +2,7 @@ class Lume < Formula
   desc "CLI and local API server to run macOS and Linux VMs on Apple Silicon"
   homepage "https://github.com/trycua"
   url "https://github.com/trycua/lume/releases/download/lume-v0.2.3/lume.tar.gz"
-  sha256 "9a488a0f8bdbb10d21cb1c73af4bb7220853860c9231f82128193c8701b99dfe"
+  sha256 "df7a98b83118b8c8c22d8d3f9bca8f0c12e5781ccb0d2ad25b68f65b78df807b"
   license "MIT"
 
   depends_on :macos
